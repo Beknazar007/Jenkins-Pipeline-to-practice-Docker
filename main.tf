d2403aa58effacfa07213f68696efd2fdea6f509
Bekanzar007 <saitovbeknazar6@gmail.com>
2022-02-27 02:09:32 +0600

    credentials try2

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ provider "aws" {
 resource "aws_instance" "instance" {
     count = 2
     ami = "ami-033b95fb8079dc481"
-    
+    instance_type = "t2.micro"
+    tags = {
+      name = "Name"
+      value = "Beki"
+    }
   
 }
