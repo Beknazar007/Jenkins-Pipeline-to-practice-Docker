3b2f2958d3ef2b7ae4200da8931cc7729e52136b
Bekanzar007 <saitovbeknazar6@gmail.com>
2022-02-27 02:01:27 +0600

    credentials try2

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "us-west-2"
-  access_key = "AKIAUQDIWPF73FW4FHOX"
-  secret_key = "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"
+
 }
 
 resource "aws_instance" "instance" {
